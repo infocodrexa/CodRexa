f7e6bb8d1c0d6c1bf2a768fad1194d7cd0b5e152
abhishekraj05 <abhishekraj995554@gmail.com>
2025-08-24 19:26:30 +0530

    backend complete

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // sab requests yahi se prefix ho jayenge
+  baseURL: "https://codrexa.onrender.com/api",
 });
 
 
